f11b565e91dc62b9d80d960682ccb8d5d9d23924
ohjongchan <jongchan_oh@tmax.co.kr>
2023-02-28 11:03:36 +0900

    skeleton code add

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,62 @@
 import { Module } from '@nestjs/common';
+import * as Joi from 'joi';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/entities/user.entity';
+import { GraphQLModule } from '@nestjs/graphql';
+import { JwtModule } from './jwt/jwt.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('dev', 'prod', 'test')
+          .required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: process.env.NODE_ENV !== 'prod',
+      logging:
+        process.env.NODE_ENV !== 'prod' && process.env.NODE_ENV !== 'test',
+      entities: [User],
+    }),
+    GraphQLModule.forRoot({
+      installSubscriptionHandlers: true,
+      autoSchemaFile: true,
+      context: ({ req, connection }) => {
+        const TOKEN_KEY = 'x-jwt';
+        return {
+          token: req ? req.headers[TOKEN_KEY] : connection.context[TOKEN_KEY],
+        };
+      },
+    }),
+    JwtModule.forRoot({
+      privateKey: process.env.PRIVATE_KEY,
+    }),
+    AuthModule,
+    UsersModule,
+    CommonModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/users/users.service.ts
@@ -0,0 +1,136 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import {
+  CreateAccountInput,
+  CreateAccountOutput,
+} from './dtos/create-account.dto';
+import { LoginInput, LoginOutput } from './dtos/login.dto';
+import { User } from './entities/user.entity';
+import { UserProfileOutput } from './dtos/user-profile.dto';
+import { JwtService } from 'src/jwt/jwt.service';
+
+import { EditProfileInput, EditProfileOutput } from './dtos/edit-profile.dto';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private readonly users: Repository<User>,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async createAccount({
+    email,
+    password,
+    nickname,
+  }: CreateAccountInput): Promise<CreateAccountOutput> {
+    try {
+      const emailExists = await this.users.findOne({ email });
+      if (emailExists) {
+        return {
+          ok: false,
+          error: 'There is a user with that email already',
+        };
+      }
+      const nicknameExists = await this.users.findOne({ nickname });
+      if (nicknameExists) {
+        return {
+          ok: false,
+          error: 'There is a user with that nickname already',
+        };
+      }
+      const user = await this.users.save(
+        this.users.create({ email, password, nickname }),
+      );
+      // TODO: send verification code
+      return { ok: true };
+    } catch (e) {
+      return { ok: false, error: "Couldn't create account" };
+    }
+  }
+
+  async login({ email, password }: LoginInput): Promise<LoginOutput> {
+    try {
+      const user = await this.users.findOne(
+        { email },
+        { select: ['id', 'password'] },
+      );
+      if (!user) {
+        return {
+          ok: false,
+          error: 'User not found',
+        };
+      }
+      const passwordCorrect = await user.checkPassword(password);
+      if (!passwordCorrect) {
+        return {
+          ok: false,
+          error: 'Wrong password',
+        };
+      }
+      const token = this.jwtService.sign(user.id);
+      return {
+        ok: true,
+        token,
+      };
+    } catch (error) {
+      return {
+        ok: false,
+        error: "Can't log user in.",
+      };
+    }
+  }
+
+  async findById(id: number): Promise<UserProfileOutput> {
+    try {
+      const user = await this.users.findOneOrFail({ id });
+
+      return {
+        ok: true,
+        user: user,
+      };
+    } catch (error) {
+      return { ok: false, error: 'User Not Found' };
+    }
+  }
+
+  async editProfile(
+    userId: number,
+    { email, password, nickname }: EditProfileInput,
+  ): Promise<EditProfileOutput> {
+    try {
+      const emailExists = await this.users.findOne({ email });
+      if (emailExists) {
+        return {
+          ok: false,
+          error: 'There is a user with that email already',
+        };
+      }
+      const nicknameExists = await this.users.findOne({ nickname });
+      if (nicknameExists) {
+        return {
+          ok: false,
+          error: 'There is a user with that nickname already',
+        };
+      }
+      const user = await this.users.findOne(userId);
+      if (email) {
+        user.email = email;
+        user.verified = false;
+        // TODO: send verify email
+      }
+      if (password) {
+        user.password = password;
+      }
+      if (nickname) {
+        user.nickname = nickname;
+      }
+      await this.users.save(user);
+      return {
+        ok: true,
+      };
+    } catch (error) {
+      return { ok: false, error: 'Could not update profile.' };
+    }
+  }
+}
